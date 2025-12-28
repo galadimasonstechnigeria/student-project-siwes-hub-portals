4648722b1b1f3e2045c3cf5d7221a4d88c57a53a
galadimasonstechnigeria <yakububellogaladima@gmail.com>
2025-12-28 14:41:46 +0100

    Update create_admin.py

--- create_admin.py
+++ create_admin.py
@@ -2,21 +2,13 @@ from app import app, db, User
 from werkzeug.security import generate_password_hash
 
 with app.app_context():
-    email = "admin@siwes.com"
-
-    existing_admin = User.query.filter_by(email=email).first()
-
-    if existing_admin:
-        print("❗ Admin already exists")
-    else:
-        admin = User(
-            fullname="Super Admin",
-            email=email,
-            password=generate_password_hash("admin123"),
-            role="admin"
-        )
-        db.session.add(admin)
-        db.session.commit()
-        print("✅ Admin created successfully")
-
-print("DONE")
+    admin = User(
+        fullname="Admin",
+        email="admin@siwes.com",
+        password=generate_password_hash("admin123"),
+        role="admin"
+    )
+    db.session.add(admin)
+    db.session.commit()
+    print("Admin created")
+    
